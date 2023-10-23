fba2abeb1f4bd99669ad180c39c8dd85ea799534
moe beydoun <moebeydoun24@yahoo.com>
2023-10-22 20:13:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+
+let newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace 'YOUR-NAME'
+
+
+document.body.appendChild(newHeader);
